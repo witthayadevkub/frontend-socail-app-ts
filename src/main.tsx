baeb5d258b25db8e8d86e9e1ad7b9144b16a8647
witthayadevkub <tigermixdev@gmail.com>
2024-03-09 14:39:29 +0700

    debug

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
 //redux
-import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store} >
-      <App />
-    </Provider>
-  </React.StrictMode >,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  throw new Error("Root element with id 'root' not found in the document.");
+}
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+// //redux
+// import { Provider } from 'react-redux'
+// import { store } from './store/store.ts'
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <Provider store={store} >
+//       <App />
+//     </Provider>
+//   </React.StrictMode >,
+// )
